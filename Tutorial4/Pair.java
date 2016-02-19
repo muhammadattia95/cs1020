49922d8a2217a2eb03b5164092bddbdbb14cf3e6
kfwong <kfwong@zoho.com>
2016-02-19 14:47:27 +0800

    Added solution for Q3: inheritance and composition.

--- Tutorial4/Pair.java
+++ Tutorial4/Pair.java
@@ -2,18 +2,30 @@ class GenericTriple1<T, U, V>{
 	private Pair<T, Pair<U, V>> triple;
 	
 	public GenericTriple1(T obj1, U obj2, V obj3){
-		//
+		triple = new Pair<T, Pair<U,V>>(obj1, new Pair<U, V>(obj2, obj3));
 	}
 
 	public Pair<T, Pair<U,V>> getTriple(){
 		return triple;
 	}
+
+	public T getLeft(){
+		return triple.getLeft();
+	}
+
+	public U getCenter(){
+		return triple.getRight().getLeft();
+	}
+
+	public V getRight(){
+		return triple.getRight().getRight();
+	}
 }
 
-class GenericSingle2<T, U, V> extends Pair<T,V>{
+class GenericTriple2<T, U, V> extends Pair<T,V>{
 	private U _objCenter;
 
-	public GenericSingle2(T pobjLeft, U pobjCenter, V pobjRight){
+	public GenericTriple2(T pobjLeft, U pobjCenter, V pobjRight){
 		super(pobjLeft, pobjRight);
 		this._objCenter = pobjCenter;
 	}
@@ -42,5 +54,11 @@ public class Pair<T, S>{
 
 	public String toString(){
 		return "("+_objLeft + ","+_objRight+")";
+	}
+
+	public static void main(String[] args){
+		// or create a Person class extends GenericTriple1 or 2
+		GenericTriple1<String, Double, Double> person1 = new GenericTriple1<String, Double, Double>("WKF", 55.5, 174.0);
+		GenericTriple2<String, Double, Double> person2 = new GenericTriple2<String, Double, Double>("KNN", 100.0, 14.5);
 	}
 }
